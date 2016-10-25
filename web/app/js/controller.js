36ee18aa774f7df5b45334cf93f714c353f7318e
Leonardo <leonardo.moreno@thread.com.br>
2016-10-25 15:41:00 -0200

    upd

--- web/app/js/controller.js
+++ web/app/js/controller.js
@@ -1,14 +1,21 @@
 angular.module('ondeTemJogoApp', [])
 .controller('pesquisa', function($scope, $http) {
 	$scope.pesquisarString = function(){
-
 		var url = "https://apiondetemjogo.herokuapp.com/api/v1/search/"+ $scope.pesquisa;
 		$http.get(url).
 		then(function(response) {
 			$scope.value = response.data;
-			console.log($scope.value);
-		});
-	}	
-});
+			$scope.valor = $scope.value[0];
+		//	console.log($scope.value[0]);
 
+		});	
+	}
+         
+})
+
+.controller('output',function($scope,$http){
+	
+	$scope.name ="Nome da Empresa";
+
+});
 
