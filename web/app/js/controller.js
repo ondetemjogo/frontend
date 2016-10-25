719d21d11ee2ab5f9c10f3db1840094f75ec8f85
RenatoSoutoMaior <renatosoutomaior18@gmail.com>
2016-10-25 19:22:00 -0200

    Mudanças no Controller

--- web/app/js/controller.js
+++ web/app/js/controller.js
@@ -1,15 +1,13 @@
 angular.module('ondeTemJogoApp', [])
 .controller('pesquisa', function($scope, $http) {
+
 	$scope.pesquisarString = function(){
-		var url = "https://apiondetemjogo.herokuapp.com/api/v1/search/"+ $scope.pesquisa;
+		var url = "http://apiondetemjogo.herokuapp.com/api/v1/search/";
 		$http.get(url).
 		then(function(response) {
-			$scope.value = response.data;
-			// $scope.valor = $scope.value[0];
-			console.log($scope.value[0]);
+		  $scope.value	= response.data;
 
-		});	
+		});
 	}
-         
-})
 
+})
