254ab3d2e8b7256dc9731010b78ab07ed68562a5
Ricardo Luiz Silveira <ricardo.silveira@gmail.com>
2016-10-28 09:22:59 -0200

    add firefox in standard browsers

--- web/karma.conf.js
+++ web/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config){
 
     frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
+    browsers : ['Chrome', 'firefox'],
 
     plugins : [
             'karma-chrome-launcher',
