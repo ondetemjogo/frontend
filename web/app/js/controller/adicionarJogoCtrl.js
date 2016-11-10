40630fb9ad4c57c846b2280aaa6ec43dc01bbdef
Arthur <arthur.santiago@matera.com>
2016-11-10 08:29:30 -0200

    Finalização da busca de brasao e jogos.

--- web/app/js/controller/adicionarJogoCtrl.js
+++ web/app/js/controller/adicionarJogoCtrl.js
@@ -1,25 +1,32 @@
 angular.module("ondeTemJogoApp").controller("adicionarJogoCtrl", function ($scope, $state, brasileiraoAPI){
-
   $scope.mytime = new Date();
-
   $scope.hstep = 1;
   $scope.mstep = 1;
-
   $scope.ismeridian = true;
+	$scope.selectJogo = [];
 
  var obterJogosFases = function (fases){
+	 $scope.jogosDisponiveis = [];
+	 var diaHoje = new Date();
+
 	 angular.forEach(fases, function(value, key) {
-		  $scope.dataJogos = value.jogos.data;
-			$scope.idJogos = value.jogos.id;
-		});
- }
+				angular.forEach( value.jogos.id, function(jogo, idJogo) {
+					  var diaJogo = new Date(jogo.data);
+					  jogo.time1 = $scope.equipesBrasileirao[jogo.time1];
+						jogo.time2 = $scope.equipesBrasileirao[jogo.time2];
+						jogo["jogoOcorreu"] = diaHoje > diaJogo;
+						$scope.jogosDisponiveis.push(jogo);
+				})
+			})
+		};
+
 
 	var carregarDadosBrasileirao = function () {
 		brasileiraoAPI.getAllInformations("2016").then(
 			function (resp) {
-				obterJogosFases(resp.data.fases);
 				$scope.equipesBrasileirao = resp.data.equipes;
-				console.log(resp);
+				obterJogosFases(resp.data.fases);
+				console.log($scope.jogosDisponiveis);
 			},
 			function (resp){
 				alert("Ocorreu um erro ao consultar os dados do brasileirão.");
@@ -28,6 +35,9 @@ angular.module("ondeTemJogoApp").controller("adicionarJogoCtrl", function ($scop
 		);
 };
 
+$scope.atribuirValor = function(valor){
+	alert('valor');
+};
 
 carregarDadosBrasileirao();
 //$state.transitionTo('home.inicio', $stateParams, {reload: true,  inherit: false, notify: true});
